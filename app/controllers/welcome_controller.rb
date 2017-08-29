28e665d00bcaf496a9be755cff21d52299c50d6b
dashingshakeel <shkeelrauf@gmail.com>
2017-08-29 12:38:15 +0500

    Added api and generate get request.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
+   require "net/http"
+   require "uri"
+
   def index
+    uri = URI.parse("http://ip-api.com/json/#{request.remote_ip}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    @response = http.request(request)
+    
   end
 end
