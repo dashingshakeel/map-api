8cacbe177254c91d6ed56eb1f8f6039bf982ef07
dashingshakeel <shkeelrauf@gmail.com>
2017-08-30 15:42:45 +0500

    Search function added.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,25 @@
 class WelcomeController < ApplicationController
    require "net/http"
    require "uri"
+   require 'open-uri'
 
   def index
-    uri = URI.parse("http://ip-api.com/json/#{request.remote_ip}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.request_uri)
-    @response = http.request(request)
-    my_hash = JSON.parse(@response.body)
-    @lat = my_hash["lat"]
-    @lon = my_hash["lon"]
+    @uri1 = URI.parse("http://ip-api.com/json").read
 
+    a=JSON.parse(@uri1)
+    @lat = a["lat"]
+
+    @lon = a["lon"]
+    @uri = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?latlng=#{@lat},#{@lon}&key=AIzaSyCnRK9xxC6VN9HjugwH0IYP6MkNWKsAxEI").read
+
+  end
+  def search
+    search_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=#{params[:query]}&key=AIzaSyCnRK9xxC6VN9HjugwH0IYP6MkNWKsAxEI"
+    details_url = "https://maps.googleapis.com/api/place/details/json"
+    @uri = URI.parse(search_url).read
+    @json = JSON.parse(@uri)
+    @place_id = @json["results"][0]["place_id"] 
+    render :json => @json
+    url ="https://www.google.com"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+  get 'search/:query' => 'welcome#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
